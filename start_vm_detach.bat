490b2abbc395cc19f6cc33c600956934822d00f4
dgolov <dmitriy.evgenyevich@gmail.com>
2024-01-24 02:02:27 +0300

    Updated start script

--- start_vm_detach.bat
+++ start_vm_detach.bat
@@ -1,3 +1,22 @@
 @echo off
+
+setlocal EnableDelayedExpansion
+
+echo Start run virtual machines
+set i=0
+
+echo Read vm_uuids.txt
+for /f "tokens=* delims=" %%a in (vm_uuids.txt) do (
+    set /a i+=1
+    set "uuid[!i!]=%%a"
+    echo Reading line - %%a
+)
+
 cd C:\Program Files\Oracle\VirtualBox
-VBoxManage.exe startvm {vmUUID} --type headless
+
+for /l %%i in (1,1,%i%) do (
+	echo Start !uuid[%%i]! 
+	VBoxManage.exe startvm {!uuid[%%i]!} --type headless
+)
+
+endlocal
